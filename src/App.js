d0c3ce4795b9e48126159d4c4e7024058cfe672f
raserrock <piyushrajput032@gmail.com>
2023-03-17 10:56:50 +0530

    complete working of app when api available for testing with pagination feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [pagenumber, setPagenumber] = useState(1);
+  const [inputval, setInputValue] = useState();
+
+  const fetchPassenger = async () => {
+    const res = await fetch(`https://api.instantwebtools.net/v1/passenger?page=1&size=10`);
+    const result = await res.json();
+    if (result && result.data)
+      setData(result.data);
+  };
+
+  useEffect(() => {
+    fetchPassenger();
+  }, [pagenumber]);
+
+  const setPages = (page) => {
+    setPagenumber(page);
+   
+  };
+  console.log(pagenumber)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length>0 &&  <div className='box'>
+      <div className='btn'>
+      
+        <input type='text' placeholder='enter page number' onChange={(e) => setInputValue(e.target.value)
+        } />
+        <button style={{ backgroundColor: 'grey' }} onClick={() => setPages(inputval)}>Go</button>
+      </div>
+
+      <>{
+        data.map((items, i) =>
+          <div className='btn' key={i}>
+            <div>{items.name}</div>
+            <div>{items.trips}</div>
+            <div>{items.airline[0].name}</div>
+          </div>
+        )
+      }
+      </>
+      <button style={{ backgroundColor: 'aqua' }} onClick={() => pagenumber > 1 ? setPagenumber(pagenumber - 1) : setPagenumber(1)}> Previos Page</button>
+      <button style={{ backgroundColor: 'aqua' }} onClick={() => setPagenumber(pagenumber + 1)}> Next Page</button>
+      </div>
+      }
+
     </div>
   );
 }
